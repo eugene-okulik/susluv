a87eff28a78a36c8ebdfc4fa510e779f12a9c1da
susluv <williamir@mail.ru>
2025-08-20 22:25:46 +0300

    fixed formatting

--- homework/susluv/homework_3/3_mean.py
+++ homework/susluv/homework_3/3_mean.py
@@ -1,5 +1,6 @@
 a = 3
 b = 4
-arithmetic_mean = (a + b)/2
+arithmetic_mean = (a + b) / 2
 geometric_mean = (a * b) ** (1 / 2)
-print('Для чисел ', a, ' и ', b, 'среднее математическое равно', arithmetic_mean, ', а среднее геометрическое -', geometric_mean)
+print('Для чисел ', a, ' и ', b, 'среднее математическое равно', arithmetic_mean, ',')
+print('а среднее геометрическое -', geometric_mean)
--- homework/susluv/homework_3/4_triangle.py
+++ homework/susluv/homework_3/4_triangle.py
@@ -1,5 +1,5 @@
 leg_1 = 3
 leg_2 = 4
-hypotenuse = (leg_1 ** 2 + leg_2 ** 2) ** (1/2)
-area = (leg_1*leg_2) / 2
+hypotenuse = (leg_1 ** 2 + leg_2 ** 2) ** (1 / 2)
+area = (leg_1 * leg_2) / 2
 print('Если катеты равны', leg_1, 'и', leg_2, ', то гипотенуза равна', hypotenuse, ', а площадь треугольника -', area)
