1d45ac8b5c850a3f446d46bed13cfb2a022e503b
susluv <williamir@mail.ru>
2025-08-20 22:19:15 +0300

    homework 3 done

---
+++ homework/susluv/homework_3/3_mean.py
@@ -0,0 +1,5 @@
+a = 3
+b = 4
+arithmetic_mean = (a + b)/2
+geometric_mean = (a * b) ** (1 / 2)
+print('Для чисел ', a, ' и ', b, 'среднее математическое равно', arithmetic_mean, ', а среднее геометрическое -', geometric_mean)
